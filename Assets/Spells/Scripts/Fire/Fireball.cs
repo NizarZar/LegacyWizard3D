31bd2efe56687ac357b2f4a171e86f8450f0681f
Nizar Zariouh <nizarzariouh05@gmail.com>
2023-04-16 02:40:02 -0400

    Structure
    Made new folders to better structure the components of the game in the editor

---
+++ Assets/Spells/Scripts/Fire/Fireball.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Spells.Scripts
+{
+    public class Fireball : Spell
+    {
+        public override void DebugPrint()
+        {
+            Debug.Log("This is a " + SpellToCast.SpellName);
+        }
+    }
+}
